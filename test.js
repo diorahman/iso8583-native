0bf60be0e357464fb39019ba8b7636021ff8bdce
Dhi Aurrahman <diorahman@rockybars.com>
2015-06-01 23:16:29 +0700

    Give some additional spaces

--- test.js
+++ test.js
@@ -15,7 +15,6 @@ describe('ISO 8583 message packing and unpacking', function(){
     iso8583.setField(41, '12345');
     iso8583.setField(42, '678901234');
     iso8583.setField(125, 'BLAH BLAH');
-    iso8583.dump();
     packedMessage = iso8583.packSync();
     packedMessage.length.should.equal(72);
     done();
@@ -24,16 +23,14 @@ describe('ISO 8583 message packing and unpacking', function(){
   it ('should unpack an ISO 8583 message', function(done) {
     var iso8583 = new ISO8583(packedMessage);
     var fields = iso8583.unpackSync();
-/*    fields[0].toString().should.equal('1234');
+    fields[0].toString().should.equal('1234');
     fields[2].toString().should.equal('1234567890123456');
-    fields[4].toString().should.equal('5699');
-    fields[11].toString().should.equal('234');
-    fields[39].toString().should.equal('4');
-    fields[41].toString().should.equal('12345');
-    fields[42].toString().should.equal('678901234');
-    fields[125].toString().should.equal('BLAH BLAH');*/
-    for (var k in fields)
-      console.log(fields[k].toString());
+    fields[4].toString().should.equal('000000005699');
+    fields[11].toString().should.equal('000234');
+    fields[39].toString().should.equal('004');
+    fields[41].toString().should.equal('12345   '); // not sure about the space
+    fields[42].toString().should.equal('678901234      '); // not sure about the space
+    fields[125].toString().should.equal('BLAH BLAH');
     done();
   });
 });
